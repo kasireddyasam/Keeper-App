78f993b70fb184181172b212046431b213a9b90a
Kasireddy Asam <122038955+kasireddyasam@users.noreply.github.com>
2024-12-16 00:13:28 +0530

    Add files via upload

---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+import React from "react";
+import notes from "../notes";
+
+function Note() {
+  return notes.map((note) => (
+    <div className="note" key={note.key}>
+      <h1>{note.title}</h1>
+      <p>{note.content}</p>
+    </div>
+  ));
+}
+
+export default Note;
